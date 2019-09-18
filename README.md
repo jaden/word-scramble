2b6a69a4587aa72a17cf64fb1eba68265a175db2
Dan Hersam <dan@hersam.com>
2019-09-18 18:01:03 -0400

    Initial version

---
+++ README.md
@@ -0,0 +1,14 @@
+# Word Scramble Game
+
+![](word-scramble.png)
+
+See if you can unscramble the word. Choose from easy, medium and hard levels.
+
+Play at [scramble.danhersam.com](https://scramble.danhersam.com)
+
+# Usage
+
+* As you type each letter the remaining letters will be shown.
+* If your guess is correct, click New Word to start over.
+* If you give up, type `0` to see the answer.
+* Hit Enter in the guess field to be given another word.
---
+++ app.js
@@ -0,0 +1,111 @@
+const crypto = window.crypto || window.msCrypto;
+
+const LEVEL = {
+  EASY: 'easy',
+  MEDIUM: 'medium',
+  HARD: 'hard',
+  ALL: 'all',
+};
+
+function getRandomNumber(length) {
+  return Math.floor(crypto.getRandomValues(new Uint32Array(1))[0] / (0xffffffff + 1) * length);
+}
+
+function scramble(word) {
+  const letters = word.split('');
+
+  for (let i = letters.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    const tmp = letters[i];
+
+    letters[i] = letters[j];
+    letters[j] = tmp;
+  }
+
+  // Handle case where scrambling results in unscrambled word
+  if (letters.join('') === word) {
+    const firstLetter = letters[0];
+    const lastIndex = letters.length - 1;
+
+    letters[0] = letters[lastIndex];
+    letters[lastIndex] = firstLetter;
+  }
+
+  return letters.join('');
+}
+
+new Vue({
+  el: '#app',
+  data: {
+    originalWord: '',
+    scrambledWord: '',
+    originalUserGuess: '',
+    difficulty: LEVEL.MEDIUM,
+    invalidGuesses: [],
+    revealCode: '0',
+  },
+
+  mounted: function () {
+    this.getScrambledWord();
+  },
+
+  methods: {
+    getScrambledWord: function () {
+      const filteredWords = words
+        .filter(word => word.length >= this.minWordLength && word.length <= this.maxWordLength);
+
+      this.originalWord = filteredWords[getRandomNumber(filteredWords.length)];
+      this.scrambledWord = scramble(this.originalWord);
+      this.originalUserGuess = '';
+
+      this.$refs.guess.focus();
+    }
+  },
+
+  computed: {
+    userGuess: function () {
+      return this.originalUserGuess.toLowerCase().trim();
+    },
+
+    remainingLetters: function () {
+      const wordLetters = this.scrambledWord.split('');
+
+      this.invalidGuesses = [];
+
+      this.userGuess.split('').forEach(guessedLetter => {
+        const i = wordLetters.indexOf(guessedLetter);
+
+        if (i >= 0) {
+          wordLetters.splice(i, 1);
+        } else {
+          this.invalidGuesses.push(guessedLetter);
+        }
+      });
+
+      if (wordLetters.length === 0) {
+        return '(None)';
+      }
+
+      return wordLetters.join('');
+    },
+
+    minWordLength: function () {
+      if (this.difficulty === LEVEL.MEDIUM) return 6;
+      if (this.difficulty === LEVEL.HARD) return 8;
+
+      return 4; // Easy and all both have 4 as minimum
+    },
+
+    maxWordLength: function () {
+      if (this.difficulty === LEVEL.EASY) return 5;
+      if (this.difficulty === LEVEL.MEDIUM) return 7;
+
+      return 10; // Hard and all both have 10 as maximum
+    },
+
+    isCorrectGuess: function () {
+      return this.originalWord === this.userGuess;
+    },
+  },
+});
